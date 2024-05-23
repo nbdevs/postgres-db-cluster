9b98b4cb7dfce0a56e05fef3d488146809c4df85
Nicholas <nicholasbojor@gmail.com>
2024-05-23 14:30:06 +0100

    Changed the loop integer index to 1 for sql identifier in copy_expert procedure call. Also syntax changes in SQL statements where modified to be compliant.

--- postgresmaster/master_sql/pre_transform.sql
+++ postgresmaster/master_sql/pre_transform.sql
@@ -14,25 +14,30 @@ BEGIN
     RAISE NOTICE 'CREATING LANDING TABLE';
     --stage 1 
     EXECUTE FORMAT('CREATE TABLE IF NOT EXISTS preprocess.%I();', table_name); -- creating landing table
+    
+    RAISE NOTICE 'ADDING COLUMNS TO LANDING TABLE';
     FOR iter IN 1..column_count
     LOOP -- loop through and populate table based on 
         EXECUTE FORMAT('ALTER TABLE preprocess.%I ADD COLUMN col_%s TEXT;', table_name, iter);
     END LOOP;
 
-    RAISE NOTICE 'ADDED COLUMNS TO LANDING TABLE';
+   RAISE NOTICE 'COPYING CSV DATA INTO TABLE';
     -- stage 2
-    EXECUTE FORMAT('COPY preprocess.%I FROM ''%s'' WITH (FORMAT CSV, DELIMITER '','', HEADER);', table_name, csv_path);
-   EXECUTE FORMAT("SELECT col_1 FROM %I LIMIT 1 INTO col_first", table_name);
+   EXECUTE FORMAT('COPY preprocess.%I FROM ''%s'' WITH (FORMAT CSV, DELIMITER '','', HEADER);', table_name, csv_path);
 
+
+   EXECUTE FORMAT("SELECT col_1 FROM %I LIMIT 1 INTO col_first;", table_name);
+
+    RAISE NOTICE 'RENAMING COLUMN NAMES WITHIN TABLE';
     SET iter TO 1;
-
-    FOR col IN EXECUTE FORMAT('SELECT UNNEST(string_to_array(TRIM(%s::text, ''()''), '','')) FROM preprocess.%s WHERE col_1 = %L', table_name, table_name, col_first)
+    FOR col IN EXECUTE FORMAT('SELECT UNNEST(string_to_array(TRIM(%s::text, ''()''), '','')) FROM preprocess.%s WHERE col_1 = %L;', table_name, table_name, col_first)
     LOOP
-        EXECUTE FORMAT('ALTER TABLE preprocess.%I RENAME COLUMN col_%s TO %s', table_name, iter, col);
+        EXECUTE FORMAT('ALTER TABLE preprocess.%I RENAME COLUMN col_%s TO %s;', table_name, iter, col);
         iter := iter + 1; -- increment counter
     END LOOP;
+    RAISE NOTICE 'DELETING COLUMN FROM TABLE'
     -- delete the columns row
-    EXECUTE FORMAT('DELETE FROM preprocess.%I WHERE %s = %L', table_name, col_first, col_first);
+    EXECUTE FORMAT('DELETE FROM preprocess.%I WHERE %s = %L;', table_name, col_first, col_first);
 
 END;
 $$ SECURITY DEFINER; -- to bypass superuser permissions 
@@ -50,9 +55,11 @@ DECLARE
 file_name_ VARCHAR(30);
 full_table_name_ VARCHAR(30);
 csv_file_ VARCHAR(200);
-number_ INTEGER := 0;
-record_count INTEGER := 0;
+number_ INTEGER;
+record_count INTEGER;
 BEGIN   
+
+    RAISE NOTICE 'CREATING TEMP TABLE';
 
     CREATE TEMP TABLE IF NOT EXISTS tmp(
         csv_file_name VARCHAR(30),
@@ -60,8 +67,9 @@ BEGIN
         csv_file_path VARCHAR(200)
     );
     
+    RAISE NOTICE 'INSERTING RECORDS INTO TABLE';
     -- inserting filenames into tmp table from array retrieved from python code
-    FOR i IN 0..no_tables
+    FOR i IN 1..no_tables -- ARRAYS START FROM 1 
     LOOP
         INSERT INTO tmp (csv_file_name) 
         VALUES (array_of_files[i]);
@@ -71,16 +79,10 @@ BEGIN
         VALUES (array_of_csv[i]);
     END LOOP;
 
-    -- cte to generate counter variable for total amount of tables to create
-    WITH loop_count AS (
-        SELECT COUNT(*) 
-        FROM tmp 
-        WHERE csv_file_name IS NOT NULL
-    )
-    SELECT * FROM loop_count INTO number_;
-
-    FOR i IN 0..number_
+    FOR j IN 1..no_tables
     LOOP
+        RAISE NOTICE 'CREATING PROCEDURE PARAMETERS';
+
         WITH file_names_ AS(
             SELECT csv_file_name
             FROM tmp
@@ -105,10 +107,14 @@ BEGIN
         )
         SELECT * FROM csv_files INTO csv_file_; -- variable for csv file directory
 
-        SELECT number_of_columns[i] INTO record_count;
+        WITH record_ AS(
+            SELECT number_of_columns[j]::INTEGER
+        )
+        SELECT * FROM record_ INTO record_count;
 
+        RAISE NOTICE 'CALLING COPY EXPERT FUNCTION';
         -- loading staging tables with data from csv files
-        EXECUTE FORMAT('CALL preprocess.copy_expert(''%s'', ''%s'', %I, ''%s'')', file_name_, csv_file_, record_count, full_table_name_);
+        EXECUTE FORMAT('CALL preprocess.copy_expert(''%s'', ''%s'', ''%s'', ''%s'')', file_name_, csv_file_, record_count, full_table_name_);
         -- removing data from temp table
         DELETE FROM tmp WHERE csv_file_name = file_name_; -- delete the filename from the table 
         DELETE FROM tmp WHERE csv_file_path = csv_file_;
